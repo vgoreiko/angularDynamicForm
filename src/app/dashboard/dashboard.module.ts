08153cab01514d7ff61b7c2398ba1f3e44390268
vgoreiko <mbox696@gmail.com>
2020-01-30 21:21:23 +0200

    base structure

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-
+import {ReactiveFormsModule} from "@angular/forms";
+import {FormlyModule} from "@ngx-formly/core";
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormlyModule,
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {FormlyFieldConfig} from "@ngx-formly/core";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  form = new FormGroup({});
+  model = {email: 'email@gmail.com'};
+  fields: FormlyFieldConfig[] = [{
+    key: 'email',
+    type: 'input',
+    templateOptions: {
+      label: 'Email address',
+      placeholder: 'Enter email',
+      required: true,
+    }
+  }];
 
-  constructor() { }
+  submit(model) {
+    console.log(model);
+  }
 
   ngOnInit() {
   }
